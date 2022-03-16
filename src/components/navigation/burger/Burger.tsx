ed158fe7aa6e4dfb9c554fa0ac5cf806ddc3b239
Trenten Grede <trenten.grede@gmail.com>
2022-03-16 00:27:18 -0500

    Added header back components and utility to make it work

---
+++ src/components/navigation/burger/Burger.tsx
@@ -0,0 +1,23 @@
+import { useState, useRef } from 'react';
+
+import BurgerIcon from './BurgerIcon';
+import BurgerMenu from './BurgerMenu';
+import useOnClickOutside from '../../../utilities/useOnClickOutside';
+
+function Burger() {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const burgerRef = useRef<HTMLDivElement>(null);
+
+  // closes menu when clicking anywhere outside of it
+  useOnClickOutside(burgerRef, () => setOpen(false));
+
+  return (
+    <div ref={burgerRef}>
+      <BurgerIcon open={open} setOpen={setOpen} />
+      <BurgerMenu open={open} setOpen={setOpen} />
+    </div>
+  );
+}
+
+export default Burger;

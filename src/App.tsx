a37c1f8309ed1bc56a9547ade142759e331b0fc0
Trenten Grede <trenten.grede@gmail.com>
2022-03-16 00:27:41 -0500

    Render Header bar component in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Input from 'components/inputs/Input';
+import HeaderBar from 'components/navigation/HeaderBar';
 
 export default function App() {
   return (
     <main>
+      <HeaderBar />
       <p>Hello World</p>
       <Input />
     </main>

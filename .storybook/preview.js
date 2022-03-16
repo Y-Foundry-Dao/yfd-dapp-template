c9ced9819acc5d446976c69fbb94d6606d005a7d
Trenten Grede <trenten.grede@gmail.com>
2022-03-16 00:02:39 -0500

    Added a global decorator for styled component theme, and added story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,19 @@
+import ThemeComponent from '../src/styles/ThemeComponent';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+      date: /Date$/
+    }
+  }
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeComponent>
+      <Story />
+    </ThemeComponent>
+  )
+];

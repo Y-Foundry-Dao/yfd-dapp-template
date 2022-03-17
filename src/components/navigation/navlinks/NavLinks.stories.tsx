872065acbd94e3430c216afec1decc8558cdf05a
Trenten Grede <trenten.grede@gmail.com>
2022-03-17 12:13:51 -0500

    Added NavLinks and Stories for it

---
+++ src/components/navigation/navlinks/NavLinks.stories.tsx
@@ -0,0 +1,27 @@
+import NavLinks from './NavLinks';
+import NavLink from '../navlink/NavLink';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+export default {
+  title: 'Navigation/NavLinks',
+  component: NavLinks,
+  subcomponents: { NavLink },
+  args: {
+    navLinks: ['link', 'link 2', 'link 3', 'link 4']
+  }
+} as ComponentMeta<typeof NavLinks>;
+
+export const NavTemplate: ComponentStory<typeof NavLinks> = (args) => {
+  return (
+    <NavLinks {...args}>
+      {args.navLinks.map((link: string) => (
+        <NavLink text={link} />
+      ))}
+    </NavLinks>
+  );
+};
+
+export const LandingPage = NavTemplate.bind({});
+LandingPage.args = {
+  navLinks: ['about', 'medium', 'join community', 'roadmap']
+};

6511053834ea173deb32bb4fa5029acd9cad4015
Trenten Grede <trenten.grede@gmail.com>
2022-03-18 23:32:49 -0500

    Created header bar and added it into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import Input from 'components/inputs/Input';
-import HeaderBar from 'components/navigation/HeaderBar';
+import HeaderBar from 'components/navigation/headerBar/HeaderBar';
+import yLogo from 'assets/yfd/logo-orange.svg';
+import { useState } from 'react';
 
 export default function App() {
+  const [open, setOpen] = useState(false);
   return (
     <main>
-      <HeaderBar />
+      <HeaderBar
+        id="home"
+        src={yLogo}
+        alt="Y Logo"
+        navLinks={['about', 'medium', 'join community', 'roadmap']}
+        open={open}
+        setOpen={setOpen}
+      />
       <p>Hello World</p>
       <Input />
     </main>
---
+++ src/components/navigation/headerBar/HeaderBar.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import HeaderBar from './HeaderBar';
+import yLogo from '../../../assets/yfd/logo-orange.svg';
+import { useState } from 'react';
+
+export default {
+  title: 'Navigation/HeaderBar',
+  component: HeaderBar,
+  args: {
+    id: 'home',
+    src: yLogo,
+    alt: 'Y Logo',
+    navLinks: ['link', 'link 2', 'link 3', 'link 4'],
+    open: false
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ margin: '-1rem' }}>
+        <Story />
+      </div>
+    )
+  ]
+} as ComponentMeta<typeof HeaderBar>;
+
+export const Template: ComponentStory<typeof HeaderBar> = (args) => {
+  const [open, setOpen] = useState(false);
+  return <HeaderBar {...args} open={open} setOpen={setOpen} />;
+};
+Template.args = {};

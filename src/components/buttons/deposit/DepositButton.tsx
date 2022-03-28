c2679b84e81851453abe14364075a954820acac1
Trenten Grede <trenten.grede@gmail.com>
2022-03-28 08:12:48 -0500

    Added more styles for the button

--- src/components/buttons/deposit/DepositButton.tsx
+++ src/components/buttons/deposit/DepositButton.tsx
@@ -18,6 +18,12 @@ function DepositButton({ children, disabled, onClick }: Props) {
 const Button = styled.button`
   background-color: ${(props) => `${props.theme.colors.blue}`};
   color: ${(props) => `${props.theme.colors.white}`};
+  border: none;
+  border-radius: 10px;
+  padding: 13px 32px;
+  font-size: 1.1rem;
+  font-weight: 700;
+  text-transform: uppercase;
 `;
 
 export default DepositButton;
